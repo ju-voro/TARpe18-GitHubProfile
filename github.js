2b75fcdde0ebd46d5606a814e8b0c495883eff7d
Julia Aasmaa <my_e_10@hotmail.com>
2020-01-14 11:52:38 +0200

    first commit

---
+++ github.js
@@ -0,0 +1,19 @@
+class GitHub {
+
+    constructor(){
+        this.client_ID = '272627243b07c8815a87';
+        this.client_secret = '0db0d2063cce8b97ecac493cd56d88f9ca18307e';
+        this.user = 'ju-voro';
+        this.apiUrl = "https://api.github.com/users/";
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUserData(){
+        let urlProfile = `${this.apiUrl}${this.user}?client_id=${this.client_ID}&client_secret=${this.client_secret}`;
+        let urlRepos = `${this.apiUrl}${this.user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_ID}&client_secret=${this.client_secret}`;
+    }
+
+
+
+}
